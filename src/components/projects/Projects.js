e797c25aa3a24bed90d8f524b22741c92309145a
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-10 09:27:11 +0300

    update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,15 +12,15 @@ const ProjectsPage = () => {
       image: littleLemon,
       description: 'The front-end project created a seamless and user-friendly reservation booking page for a restaurant. It featured an intuitive form for entering details like date, time, guest count, and preferences. Real-time data integration ensured availability visibility, and feedback mechanisms guided users with validation messages. After submission, users received a reassuring confirmation message and could opt for email or SMS confirmations. The project simplified the reservation process, offering a user-friendly interface and improving customer satisfaction.',
       githubLink: 'https://github.com/charlespaul1/capstone_project',
-      previewLink: 'https://project1.com',
+      previewLink: 'https://charlespaul1-github-io-pfzx.vercel.app/',
     },
     {
       id: 2,
       title: 'To-do App',
       image: todo,
       description: 'This is the description of Project 2.',
-      githubLink: 'https://github.com/project2',
-      previewLink: 'https://project2.com',
+      githubLink: 'https://github.com/charlespaul1/TODO_app/tree/main/todo_app',
+      previewLink: 'https://todo-app-pi-lyart.vercel.app/',
     },
     {
       id: 3,
